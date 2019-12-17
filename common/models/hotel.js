0eed9ffadc47f652390195351303124b4f2ff8e4
Mahesh <jagzmz@gmail.com>
2019-12-17 23:38:34 +0530

    Initial Commit

---
+++ common/models/hotel.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = function(Hotel) {
+
+    Hotel.validatesUniquenessOf('name',{message:"exist"})
+    
+
+    Hotel.findHotelById =(id)=>{
+        
+        return Hotel.findOne({where:{id}})
+        .then(e=>{
+            if(e==null) {throw new Error("Hotel not found")}
+            else return e
+
+        })
+    }
+
+    Hotel.findIfFeatureExist= (id)=>{
+
+        return app.models.Feature.findOne({where:{id}})
+        .then(e=>{
+            if(e==null) {throw new Error("Feature not found")}
+            else return e
+
+        })
+
+        
+    }
+
+
+    
+    
+
+    Hotel.findFeatureById=async (hid,fid,cb)=>{
+        try {
+            var hotelNode=await Hotel.findHotelById(hid)  //Hotel should exist
+            var featureNode=await Hotel.findIfFeatureExist(fid)  //Feature should exist
+            hotelNode.features.push(fid)
+            hotelNode.features= [...new Set(hotelNode.features)]
+            hotelNode.save()
+            
+            return hotelNode
+
+        } catch (error) {
+            return error
+        }
+
+    }
+
+    Hotel.remoteMethod('findFeatureById',{
+        accepts: [
+            {arg: 'id', type: 'string',required:true},
+            {arg: 'fid', type: 'string',required:true}
+        ],
+        returns: {arg:'data',type: 'object',root:true},
+        http: {path: '/:id/addFeature/:fid', verb: 'put'}
+    })
+
+
+};
---
+++ server/boot/root.js
@@ -0,0 +1,27 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-workspace
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+module.exports = function(server) {
+  // Install a `/` route that returns server status
+  var router = server.loopback.Router();
+  router.get('/', server.loopback.status());
+  server.use(router);
+  if (!('toJSON' in Error.prototype))
+  Object.defineProperty(Error.prototype, 'toJSON', {
+      value: function () {
+          var alt = {};
+
+          Object.getOwnPropertyNames(this).forEach(function (key) {
+              alt[key] = this[key];
+          }, this);
+
+          return alt;
+      },
+      configurable: true,
+      writable: true
+  });
+};
